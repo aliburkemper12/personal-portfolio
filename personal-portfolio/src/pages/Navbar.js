350fe784107f7dd3aaadbbfad9b2a176f2167521
Ali Burkemper <aliburkemper12@gmail.com>
2025-06-07 17:39:24 -0500

    disabled CI

--- personal-portfolio/src/pages/Navbar.js
+++ personal-portfolio/src/pages/Navbar.js
@@ -3,15 +3,15 @@ export default function Navbar() {
                 <nav className="navbar sticky-top">
                     <div className="col-3 mx-4">
                         <div className="name">
-                            <a className="nav-name">Ali Burkemper</a>
+                            <a className="nav-name" target="_blank" rel="noreferrer">Ali Burkemper</a>
                         </div>
                     </div>
                     <div className="col-6 mx-3">
                         <div id="navbar-link">
-                            <a className="nav-text" href="#home"><span className="font-link">Home</span></a>
-                            <a className="nav-text" href="#about"><span className="font-link">About</span></a>
-                            <a className="nav-text" href="#projects"><span className="font-link">Projects</span></a>
-                            <a className="nav-text" href="#contact"><span className="font-link">Contact</span></a>
+                            <a className="nav-text" href="#home" target="_blank" rel="noreferrer"><span className="font-link">Home</span></a>
+                            <a className="nav-text" href="#about" target="_blank" rel="noreferrer"><span className="font-link">About</span></a>
+                            <a className="nav-text" href="#projects" target="_blank" rel="noreferrer"><span className="font-link">Projects</span></a>
+                            <a className="nav-text" href="#contact" target="_blank" rel="noreferrer"><span className="font-link">Contact</span></a>
                         </div>
                     </div>
                 </nav>
